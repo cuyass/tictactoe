2bafff02144c25b13875dc5b93640562a28d8b5b
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-03-10 09:49:11 +0100

    started drawing board

--- src/main/java/com/tictactoe/App.java
+++ src/main/java/com/tictactoe/App.java
@@ -1,13 +1,27 @@
 package com.tictactoe;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        byte n = 3;
+        char[][] board = new char[n][n];
+        for(byte i = 0; i < n; i++){
+            for(byte j = 0; j < n; j++){
+                board[i][j] = '-';
+            }
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("¡Hola! ¿Estás a punto para jugar? Porque yo sí");
+        
+        drawBoard(board);
+
+    }
+
+    public static void drawBoard(){
+        System.out.println("");
     }
 }
