d47a93a10805a9869eb77ac3e52d6f4f9a245a08
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-03-14 15:57:59 +0100

    added functional tests

--- src/test/java/com/tictactoe/AppTest.java
+++ src/test/java/com/tictactoe/AppTest.java
@@ -1,20 +1,57 @@
 package com.tictactoe;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testBoardIsFull_shouldReturnTrue(){
+
+        char[][] board = {
+            {'X', 'O', 'X'},
+            {'O', 'O', 'X'},
+            {'X', 'X', 'O'}
+        };
+        assertTrue(App.boardIsFull(board));
     }
+    
+    @Test
+    public void testBoardIsNotFull_shouldReturnFalse(){
+
+        char[][] board = {
+            {'-', 'O', 'X'},
+            {'O', 'O', 'X'},
+            {'X', 'X', 'O'}
+        };
+        assertFalse(App.boardIsFull(board));
+    }
+
+    @Test 
+    public void testPlayerHasWon_row(){
+
+        char[][] board = {
+            {'-', 'O', 'O'},
+            {'O', '-', 'X'},
+            {'X', 'X', 'X'}
+        };
+        assertEquals('X', App.playerHasWon(board));
+    }
+
+    @Test 
+    public void testPlayerHasWon_column(){
+
+        char[][] board = {
+            {'-', 'O', '-'},
+            {'-', 'O', 'X'},
+            {'X', 'O', 'X'}
+        };
+        assertEquals('O', App.playerHasWon(board));
+    }
+   
 }
